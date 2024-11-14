75770ab11b37be2385d2bc8b10cde49e0e681865
Shailenkaushik <107068730+Shailenkaushik@users.noreply.github.com>
2024-11-15 02:01:51 +0530

    updated new feature section, used hover:scale

--- ui-learning/src/App.jsx
+++ ui-learning/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Feature from './components/Feature'
 import Feature2 from './components/Feature2'
+import Feature3 from './components/Feature3'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
     <Hero></Hero>
     <Feature></Feature>
     <Feature2></Feature2>
+    <Feature3></Feature3>
     </>
   )
 }
---
+++ ui-learning/src/components/Feature3.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Feature3() {
+  return (
+    <div className='relative'>
+
+        <img  className='absolute left-[200px] top-6 z-10 ' src="download.png" alt="" />
+        <img  className='absolute left-[600px] top-24 z-10' src="download.png" alt="" />
+    <div className='w-7/12 grid grid-cols-3  gap-y-20 gap-x-7 m-4 p-4 mx-auto z-100'>
+     
+
+        <div className='flex justify-center items-center'>
+            <h3 className='text-3xl font-bold'>New in the <br></br> <span className='text-blue-500'>Razorpay</span> Product <br></br>Suite</h3>
+        </div>
+        <div className='flex flex-col items-start bg-white gap-y-2  hover:scale-105 z-10 transition-all duration-200'>
+            <img src="razorpayXicon.svg" alt="" />
+            <h3 className='text-xl font-bold'>Coperates Card</h3>
+            <p className='text-small opacity-80'>Simplify your recurring, international and team expenses with savings on every spend. Save upto 10 lacs every month.</p>
+            <button className='border-solid border-2  rounded-md p-2 hover:bg-blue-200 transition-all duration-200  font-bold text-blue-500'>Know More</button>
+        </div>
+        <div className='flex flex-col items-start gap-y-2  hover:scale-105 transition-all duration-200'>
+            <img src="razorpayXicon.svg" alt="" />
+            <h3 className='text-xl font-bold'>Coperates Card</h3>
+            <p className='text-small opacity-80'>Simplify your recurring, international and team expenses with savings on every spend. Save upto 10 lacs every month.</p>
+            <button className='border-solid border-2  rounded-md p-2 hover:bg-blue-200 transition-all duration-200  font-bold text-blue-500'>Know More</button>
+        </div>
+        <div className='flex flex-col items-start gap-y-2  hover:scale-105 transition-all duration-200'>
+            <img src="razorpayXicon.svg" alt="" />
+            <h3 className='text-xl font-bold'>Coperates Card</h3>
+            <p className='text-small opacity-80'>Simplify your recurring, international and team expenses with savings on every spend. Save upto 10 lacs every month.</p>
+            <button className='border-solid border-2  rounded-md p-2 hover:bg-blue-200 transition-all duration-200  font-bold text-blue-500'>Know More</button>
+        </div>
+        <div className='flex flex-col items-start gap-y-2  hover:scale-105 transition-all duration-200'>
+            <img src="razorpayXicon.svg" alt="" />
+            <h3 className='text-xl font-bold'>Coperates Card</h3>
+            <p className='text-small opacity-80'>Simplify your recurring, international and team expenses with savings on every spend. Save upto 10 lacs every month.</p>
+            <button className='border-solid border-2  rounded-md p-2 hover:bg-blue-200 transition-all duration-200  font-bold text-blue-500'>Know More</button>
+        </div>
+        <div className='flex flex-col items-start gap-y-2  hover:scale-105 transition-all duration-200'>
+            <img src="razorpayXicon.svg" alt="" />
+            <h3 className='text-xl font-bold'>Coperates Card</h3>
+            <p className='text-small opacity-80'>Simplify your recurring, international and team expenses with savings on every spend. Save upto 10 lacs every month.</p>
+            <button className='border-solid border-2  rounded-md p-2 hover:bg-blue-200 transition-all duration-200  font-bold text-blue-500'>Know More</button>
+        </div>
+
+    </div>
+    </div>
+  )
+}
